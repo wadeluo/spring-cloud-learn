2eec86f52febf4ea775ddba05b53ead6bca3450e
hjb <hjbbjh0521@126.com>
2018-05-16 14:53:53 +0800

    第一次提交（git配置中心）

---
+++ ms-configClient/src/main/java/com/hjb/spring/cloud/ConfigClientMain.java
@@ -0,0 +1,18 @@
+package com.hjb.spring.cloud;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author haojingbin
+ * @Description:启动入口类
+ * @Date 2018/5/16
+ * @Modified By:
+ */
+
+@SpringBootApplication
+public class ConfigClientMain {
+
+    public static void main(String[] args){
+
+    }
+}
